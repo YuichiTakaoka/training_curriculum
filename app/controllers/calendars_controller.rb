c7f5464c08289511ea24df0007cce73a2343533e
YuichiTakaoka <impression96722697@gmail.com>
2021-12-02 21:46:49 +0900

    Update calendars_controller.rb

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -23,7 +23,11 @@ class CalendarsController < ApplicationController
 
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
     @todays_date = Date.today
+<<<<<<< Updated upstream
     # 例) 今日が2月1日の場合・・・ Date.today.day => 1日
+=======
+    # 例) 日が2月1日の場合・・・ Date.today.day => 1日
+>>>>>>> Stashed changes
 
     @week_days = []
 
